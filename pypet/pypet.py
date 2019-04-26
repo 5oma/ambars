894173e06ac3d3864ffc560b9438fd1d0524fd6c
5oma <ariel.carbajal.contacto@gmail.com>
2019-04-26 01:10:06 -0500

    Ya nació nuestro axolote virtual.

--- pypet/pypet.py
+++ pypet/pypet.py
@@ -1,26 +1,32 @@
 # -*- coding: utf-8 -*-
 
-photo = "W(o_.._o)W"
-name = "Axolotl, el mágico anfibio mexicano." 
-age = 5
-weight = 5.5
-hungry = True
+import random
+
+axolotl = {
+    "photo": "W(o_.._o)W", 
+    "name": "Axólotl, el mágico anfibio mexicano.",
+    "weight": 1.1,
+    "age" : 2,
+    "hungry": True,
+    "phrases": ["Soy el ser de las profundidades.", "La naturaleza es el camino de regreso a casa.", "Sólo toma el camino más natural a tu alcance.", "Flow system", "Hack, amigo, hack!"] 
+}
+
+
+print(axolotl["photo"])
 
 def startup_pypet():
     print("Hola! Recuerda cuidar tu axolotl virtual.")
  
 
 def pypet_stats():
-    print("Hola, mi nombre es " + name)
-    print(name + " pesa " + str(weight) + " kilogramos ")
+    print("Hola, mi nombre es " + axolotl["name"])
+    print("Peso: " + str(axolotl["weight"]))
 
-if hungry: 
+if axolotl["hungry"]: 
     print("Tu axolote tiene hambre.")
 else: 
     print("Estoy lleno. ¡Gracias!")
 
-startup_pypet()
-pypet_stats()
 
 terminate = False
 
@@ -36,13 +42,16 @@ while not terminate:
         pypet_stats()
     
     elif user_input == "alimentar":
-	weight = weight + 0.1
+	axolotl["weight"] = axolotl["weight"] + 0.1
         hungry = False
         print("¡Yum! ¡Qué rico!") 
+
+    elif user_input == "textear":
+	print random.choice(axolotl["phrases"])
  
     else:
          print("Lo siento, hubo un error.")
 
 print "¡Hasta pronto!"
-
+print axolotl["photo"]
 
